06fbefe96185c1f1f20572ad372625f09db9db86
omerfarukaras <omerfarukaras16@gmail.com>
2026-06-15 14:38:26 +0300

    Initial Commit

---
+++ NotchAgent/NotchAgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchAgentApp.swift
+//  NotchAgent
+//
+//  Created by elixxrades on 15.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct NotchAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
